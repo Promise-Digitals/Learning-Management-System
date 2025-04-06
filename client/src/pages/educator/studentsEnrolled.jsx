d1555aa4343ab4990572d78d7ee6cfd846847835
Promise-Digitals <promiseedeh025@gmail.com>
2025-04-06 22:32:10 +0100

    setted up routing

---
+++ client/src/pages/educator/studentsEnrolled.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const studentsEnrolled = () => {
+    return (
+        <div>
+            <h1>Students Enrolled Page</h1>
+        </div>
+    )
+}
+
+export default studentsEnrolled
