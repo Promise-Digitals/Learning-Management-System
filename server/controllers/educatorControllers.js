c7781def1ef82da76f04a0b5799412893a1a2a60
Promise-Digitals <promiseedeh025@gmail.com>
2025-05-15 20:54:26 +0100

    dashboard and enrolled students API

--- server/controllers/educatorControllers.js
+++ server/controllers/educatorControllers.js
@@ -1,4 +1,5 @@
 import Course from "../models/course.js"
+import { Purchase } from "../models/purchase.js"
 import User from "../models/user.js"
 import {v2 as cloudinary} from 'cloudinary'
 
@@ -74,6 +75,92 @@ export const getEducatorCourses = async (req, res) => {
         res.json({
             success: true,
             courses
+        })
+
+    } catch (error) {
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+
+// Get Educator Dashboard Data(Total Earnings, Enrolled Students, No of courses)
+export const educatorDashboardData = async (req, res) => {
+    try {
+
+        const educatorId = req.user._id;
+        const courses = await Course.find({educator: educatorId})
+        const totalCourses = courses.length;
+
+        const courseIds = courses.map(course => course._id);
+
+        // Calculate total earnings from purchases
+        const purchases = await Purchase.find({
+            courseId: {$in: courseIds},
+            status: 'completed'
+        });
+
+        const totalEarnings = purchases.reduce((sum, purchase) => sum + purchase.amount, 0)
+
+        // collect unique enrolled students Ids with their course titles
+        const enrolledStudentsData = [];
+
+        for(const course of courses){
+            const students = await User.find({
+                _id: {$in: course.enrolledStudents}
+            }, 'name profile');
+
+            students.forEach(student => {
+                enrolledStudentsData.push({
+                    courseTitle: course.courseTitle,
+                    student
+                });
+            });
+        }
+
+        res.json({
+            success: true,
+            dashboardData: {
+                totalEarnings,
+                enrolledStudentsData,
+                totalCourses
+            }
+        })
+        
+    } catch (error) {
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+
+// Get Enrolled Students Data with Purchase Data
+export const getEnrolledStudentsData = async (req, res) => {
+    try {
+
+        const educatorId = req.user._id;
+        const courses = await Course.find({educator: educatorId})
+
+        const courseIds = courses.map(course => course._id);
+
+        const purchases = await Purchase.find({
+            courseId: {$in: courseIds},
+            status: 'completes'
+        }).populate('userId', 'name profile').populate('courseId', 'courseTitle')
+
+        const enrolledStudents = purchases.map(purchase => ({
+            student: purchase.userId,
+            courseTitle: purchase.courseId.courseTitle,
+            purchaseDate: purchase.createdAt
+        }));
+
+        res.json({
+            success: true,
+            enrolledStudents
         })
 
     } catch (error) {
--- server/routes/educatorRoutes.js
+++ server/routes/educatorRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addCourse, getEducatorCourses, updateRoleToEducator } from '../controllers/educatorControllers.js'
+import { addCourse, educatorDashboardData, getEducatorCourses, getEnrolledStudentsData, updateRoleToEducator } from '../controllers/educatorControllers.js'
 import { ensureAuthenticated } from '../middlewares/auth.js'
 import upload from '../configs/multer.js'
 import { protectEducator } from '../middlewares/educator.js'
@@ -10,7 +10,9 @@ const router = express.Router()
 // Add Educator Role
 router.get('/update-role', ensureAuthenticated, updateRoleToEducator)
 router.post('/add-course', upload.single('image'), protectEducator, addCourse)
-router.get('/courses',ensureAuthenticated, protectEducator, getEducatorCourses)
+router.get('/courses', ensureAuthenticated, protectEducator, getEducatorCourses)
+router.get('/dashboard', ensureAuthenticated, protectEducator, educatorDashboardData)
+router.get('/enrolled-students', ensureAuthenticated, protectEducator, getEnrolledStudentsData)
 
 
 export default router;
